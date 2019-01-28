f7f1ccf010e6c485e51c4ae971f2aebc49249d11
rakaadli <Raka_adli@hotmail.com>
2019-01-28 15:53:10 +0700

    haha

--- coba doang.py
+++ coba doang.py
@@ -1,37 +1,37 @@
-num1 = int(input('ketikan bilangan pertama:'))
-num2 = int(input('kerikan bilangan kedua'))
+# num1 = int(input('ketikan bilangan pertama:'))
+# num2 = int(input('kerikan bilangan kedua'))
 
-def hitungKPK(a,b):
-    x = 1
-    y = 1
-    p = a*x
-    q = b*y
-    while p!= q:
-        while p>q:
-            y = y+1
-            q = b*y
-        while p<q:
-            x = x+1
-            p = a*x
-    if p==q:
-        print (p)
-
-
+# def hitungKPK(a,b):
+#     x = 1
+#     y = 1
+#     p = a*x
+#     q = b*y
+#     while p!= q:
+#         while p>q:
+#             y = y+1
+#             q = b*y
+#         while p<q:
+#             x = x+1
+#             p = a*x
+#     if p==q:
+#         print (p)
 
 
 
 
-def hitungFPB(a,b):
 
-    if a > b:
-        smaller = b
-    else:
-        smaller = a
-    for i in range(1, smaller+1):
-        if((a % i == 0) and (b % i == 0)):
-            fpb = i
+
+# def hitungFPB(a,b):
+
+#     if a > b:
+#         smaller = b
+#     else:
+#         smaller = a
+#     for i in range(1, smaller+1):
+#         if((a % i == 0) and (b % i == 0)):
+#             fpb = i
             
-    return fpb
+#     return fpb
 
-# print("KPK dari" + str(n)
-print("FPB dari"+ str(num1),"dan", str(num2)," =", hitungFPB(num1, num2))
+# # print("KPK dari" + str(n)
+# print("FPB dari"+ str(num1),"dan", str(num2)," =", hitungFPB(num1, num2))
--- soal1.py
+++ soal1.py
@@ -1,23 +1,31 @@
 #LOGIC
-def FPB(a,b): # menentukan FPB dari dua bilangan
-    batas = a # batas pencarian FPB
+def FPB(a,b): 
+    batas = a 
     if b>a:
-        batas = b # maka b menjadi batas pencarian
+        batas = b 
 
-    fpb = 1 # FPB sementara
-    for i in range(1,batas+1): # mencari FPB
-        if (a%i==0) and (b%i==0): # jika a dan b keduanya habis dibagi i
-            fpb = i # maka i menjadi fpb sementara
+    fpb = 1 
+    for i in range(1,batas+1): 
+        if (a%i==0) and (b%i==0): 
+            fpb = i 
 
-    return fpb # kembalikan nilai fpb
+    return fpb 
 
-def KPK(a,b): # menentukan KPK dari dua bilangan
-    return int((a*b)/(FPB(a,b))) # menggunakan rumus yang diberikan
+def KPK(a,b): 
+    return int((a*b)/(FPB(a,b))) 
 
 # INPUT
-A = int(input("Masukkan bilangan A: "))
-B = int(input("Masukkan bilangan B: "))
+try:
+    A = int(input("Masukkan bilangan A: "))
+except (ValueError,TypeError,KeyboardInterrupt,KeyError):
+    print('ndak bisaaa')
+
+try:
+    B = int(input("Masukkan bilangan B: "))
+except (ValueError,TypeError,KeyboardInterrupt,KeyError):
+    print('ndak bisaaa')
+
 
 # OUTPUT
-print("KPK dari " + str(A) + " dan " + str(B) + "adalah " + str(KPK(A,B)) + ".")
+print("KPK dari " + str(A) + " dan " + str(B) + " adalah " + str(KPK(A,B)) + ".")
 print("FPB dari "+ str(A),"dan", str(B),"=", FPB(A, B))
